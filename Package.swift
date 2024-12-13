6722a9dafbf04576ab12cb23be692f3255543214
Sinon <34333654+Catta1997@users.noreply.github.com>
2024-12-13 10:29:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,34 +1,16 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+
 import PackageDescription
 
 let package = Package(
     name: "CachedAsyncImage",
-    platforms: [
-        // Add support for all platforms starting from a specific version.
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7),
-        .visionOS(.v1)
-    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CachedAsyncImage",
-            targets: ["CachedAsyncImage"]
-        )
+        .library(name: "CachedAsyncImage", targets: ["CachedAsyncImage"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CachedAsyncImage"
-        ),
-        .testTarget(
-            name: "CachedAsyncImageTests",
-            dependencies: ["CachedAsyncImage"]
-        )
+        .target(name: "CachedAsyncImage"),
+        .testTarget(name: "CachedAsyncImageTests", dependencies: ["CachedAsyncImage"])
     ]
 )
